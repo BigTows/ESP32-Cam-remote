f12dbb31f4dc4cc953436757a4798dee1fbbe099
Alexander Chapchuk <BigTows@yandex.ru>
2020-03-11 15:50:54 +0300

    Create ESP32-Cam_HTTPS.ino

---
+++ ESP32-Cam_HTTPS.ino
@@ -0,0 +1,287 @@
+/**
+ * This code based on https://github.com/fustyles/Arduino/blob/master/ESP32-CAM_Linenotify/ESP32-CAM_Linenotify.ino
+ * Author:  ChungYi Fu (Kaohsiung, Taiwan)
+ * Thx man :)
+ * 
+ * Author of this: Alexander @BigTows Chapchuk
+ */
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
+
+#include "esp_camera.h"
+
+//Pick your camera model
+//#define CAMERA_MODEL_WROVER_KIT
+//#define CAMERA_MODEL_ESP_EYE
+//#define CAMERA_MODEL_M5STACK_PSRAM
+//#define CAMERA_MODEL_M5STACK_WIDE
+//#define CAMERA_MODEL_AI_THINKER - ESP32 CAM
+
+// Enter your WiFi ssid and password
+const char *ssid = "SSID";   //your network SSID
+const char *password = "PASSWORD";   //your network password
+
+String token = "TOKEN_FOR_AUTH"; //Token for authorization.
+const char *host = "Host";
+int port = 443; //default port for https connection is 443; http 80
+
+
+
+#if defined(CAMERA_MODEL_WROVER_KIT)
+#define PWDN_GPIO_NUM    -1
+#define RESET_GPIO_NUM   -1
+#define XCLK_GPIO_NUM    21
+#define SIOD_GPIO_NUM    26
+#define SIOC_GPIO_NUM    27
+
+#define Y9_GPIO_NUM      35
+#define Y8_GPIO_NUM      34
+#define Y7_GPIO_NUM      39
+#define Y6_GPIO_NUM      36
+#define Y5_GPIO_NUM      19
+#define Y4_GPIO_NUM      18
+#define Y3_GPIO_NUM       5
+#define Y2_GPIO_NUM       4
+#define VSYNC_GPIO_NUM   25
+#define HREF_GPIO_NUM    23
+#define PCLK_GPIO_NUM    22
+
+#elif defined(CAMERA_MODEL_ESP_EYE)
+#define PWDN_GPIO_NUM    -1
+#define RESET_GPIO_NUM   -1
+#define XCLK_GPIO_NUM    4
+#define SIOD_GPIO_NUM    18
+#define SIOC_GPIO_NUM    23
+
+#define Y9_GPIO_NUM      36
+#define Y8_GPIO_NUM      37
+#define Y7_GPIO_NUM      38
+#define Y6_GPIO_NUM      39
+#define Y5_GPIO_NUM      35
+#define Y4_GPIO_NUM      14
+#define Y3_GPIO_NUM      13
+#define Y2_GPIO_NUM      34
+#define VSYNC_GPIO_NUM   5
+#define HREF_GPIO_NUM    27
+#define PCLK_GPIO_NUM    25
+
+#elif defined(CAMERA_MODEL_M5STACK_PSRAM)
+#define PWDN_GPIO_NUM     -1
+#define RESET_GPIO_NUM    15
+#define XCLK_GPIO_NUM     27
+#define SIOD_GPIO_NUM     25
+#define SIOC_GPIO_NUM     23
+
+#define Y9_GPIO_NUM       19
+#define Y8_GPIO_NUM       36
+#define Y7_GPIO_NUM       18
+#define Y6_GPIO_NUM       39
+#define Y5_GPIO_NUM        5
+#define Y4_GPIO_NUM       34
+#define Y3_GPIO_NUM       35
+#define Y2_GPIO_NUM       32
+#define VSYNC_GPIO_NUM    22
+#define HREF_GPIO_NUM     26
+#define PCLK_GPIO_NUM     21
+
+#elif defined(CAMERA_MODEL_M5STACK_WIDE)
+#define PWDN_GPIO_NUM     -1
+#define RESET_GPIO_NUM    15
+#define XCLK_GPIO_NUM     27
+#define SIOD_GPIO_NUM     22
+#define SIOC_GPIO_NUM     23
+
+#define Y9_GPIO_NUM       19
+#define Y8_GPIO_NUM       36
+#define Y7_GPIO_NUM       18
+#define Y6_GPIO_NUM       39
+#define Y5_GPIO_NUM        5
+#define Y4_GPIO_NUM       34
+#define Y3_GPIO_NUM       35
+#define Y2_GPIO_NUM       32
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     26
+#define PCLK_GPIO_NUM     21
+
+#elif defined(CAMERA_MODEL_AI_THINKER)
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+
+#else
+#error "Camera model not selected"
+#endif
+
+WiFiClientSecure wifiClient;
+
+void setup() {
+    WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
+
+    Serial.begin(115200);
+    delay(10);
+
+    WiFi.mode(WIFI_STA);
+
+    Serial.println("");
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+    WiFi.begin(ssid, password);
+
+    long int StartTime = millis();
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        if ((StartTime + 10000) < millis()) break;
+    }
+
+    Serial.println("");
+    Serial.println("STAIP address: ");
+    Serial.println(WiFi.localIP());
+
+    Serial.println("");
+
+    if (WiFi.status() != WL_CONNECTED) {
+        Serial.println("Reset");
+
+        ledcAttachPin(4, 3);
+        ledcSetup(3, 5000, 8);
+        ledcWrite(3, 10);
+        delay(200);
+        ledcWrite(3, 0);
+        delay(200);
+        ledcDetachPin(3);
+
+        delay(1000);
+        ESP.restart();
+    } else {
+        ledcAttachPin(4, 3);
+        ledcSetup(3, 5000, 8);
+        for (int i = 0; i < 5; i++) {
+            ledcWrite(3, 10);
+            delay(200);
+            ledcWrite(3, 0);
+            delay(200);
+        }
+        ledcDetachPin(3);
+    }
+
+    camera_config_t config;
+    config.ledc_channel = LEDC_CHANNEL_0;
+    config.ledc_timer = LEDC_TIMER_0;
+    config.pin_d0 = Y2_GPIO_NUM;
+    config.pin_d1 = Y3_GPIO_NUM;
+    config.pin_d2 = Y4_GPIO_NUM;
+    config.pin_d3 = Y5_GPIO_NUM;
+    config.pin_d4 = Y6_GPIO_NUM;
+    config.pin_d5 = Y7_GPIO_NUM;
+    config.pin_d6 = Y8_GPIO_NUM;
+    config.pin_d7 = Y9_GPIO_NUM;
+    config.pin_xclk = XCLK_GPIO_NUM;
+    config.pin_pclk = PCLK_GPIO_NUM;
+    config.pin_vsync = VSYNC_GPIO_NUM;
+    config.pin_href = HREF_GPIO_NUM;
+    config.pin_sscb_sda = SIOD_GPIO_NUM;
+    config.pin_sscb_scl = SIOC_GPIO_NUM;
+    config.pin_pwdn = PWDN_GPIO_NUM;
+    config.pin_reset = RESET_GPIO_NUM;
+    config.xclk_freq_hz = 10000000;
+    config.pixel_format = PIXFORMAT_JPEG;
+    //init with high specs to pre-allocate larger buffers
+    if (psramFound()) {
+        config.frame_size = FRAMESIZE_UXGA;
+        config.jpeg_quality = 10;  //0-63 lower number means higher quality
+        config.fb_count = 2;
+    } else {
+        config.frame_size = FRAMESIZE_SVGA;
+        config.jpeg_quality = 12;  //0-63 lower number means higher quality
+        config.fb_count = 1;
+    }
+
+    // camera init
+    esp_err_t err = esp_camera_init(&config);
+    if (err != ESP_OK) {
+        Serial.printf("Camera init failed with error 0x%x", err);
+        delay(1000);
+        ESP.restart();
+    }
+
+    //drop down frame size for higher initial frame rate
+    sensor_t *s = esp_camera_sensor_get();
+    s->set_framesize(s, FRAMESIZE_XGA);  // UXGA|SXGA|XGA|SVGA|VGA|CIF|QVGA|HQVGA|QQVGA
+}
+
+void loop() {
+    if (!wifiClient.connected()) {
+        Serial.println("Connect to " + String(host) + ":" + port);
+        if (wifiClient.connect(host, port)) {
+            Serial.println("Connection successful");
+        } else {
+            delay(1000);
+            return;
+        }
+    }
+    sendImageFromCamera();
+}
+
+void sendImageFromCamera() {
+    String getAll = "", getBody = "";
+
+    camera_fb_t *fb = NULL;
+    fb = esp_camera_fb_get();
+    if (!fb) {
+        Serial.println("Camera capture failed");
+        delay(1000);
+        ESP.restart();
+        return;
+    }
+
+    String head = "--ContentImage\r\nContent-Disposition: form-data; name=\"imageFile\"; filename=\"esp32-cam.jpg\"\r\nContent-Type: image/jpeg\r\n\r\n";
+    String tail = "\r\n--ContentImage--\r\n";
+
+    uint16_t imageLen = fb->len;
+    uint16_t extraLen = head.length() + tail.length();
+    uint16_t totalLen = imageLen + extraLen;
+
+    wifiClient.println("POST /api/esp/v1/camera HTTP/1.1");
+    wifiClient.println("Connection: keep-alive");
+    wifiClient.println("Host: " + String(host));
+    wifiClient.println("Authorization: Bearer " + token);
+    wifiClient.println("Content-Length: " + String(totalLen));
+    wifiClient.println("Content-Type: multipart/form-data; boundary=ContentImage");
+    wifiClient.println();
+    wifiClient.print(head);
+
+    uint8_t *fbBuf = fb->buf;
+    size_t fbLen = fb->len;
+    for (size_t n = 0; n < fbLen; n = n + 1024) {
+        if (n + 1024 < fbLen) {
+            wifiClient.write(fbBuf, 1024);
+            fbBuf += 1024;
+        } else if (fbLen % 1024 > 0) {
+            size_t remainder = fbLen % 1024;
+            wifiClient.write(fbBuf, remainder);
+        }
+    }
+
+    wifiClient.print(tail);
+    esp_camera_fb_return(fb);
+    while (wifiClient.available()) {
+        char c = wifiClient.read();
+    }
+    Serial.println("Request sent");
+}
